068b48a71d3694f0f76ec24ac381821b0103b171
Misaelcs <misaelcsauceda@gmail.com>
2022-11-07 16:38:47 -0300

    Alterações

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import './App.css';
+import React from 'react';
 import Header from './components/Header.js';
 import Display from './components/Display.js';
 import ContentSection from './components/ContentSection.js';
 import LocationSection from './components/LocationSection.js';
 import Contact from './components/Contact.js';
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Display />
-      <ContentSection />
-      <LocationSection />
-      <Contact />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Display />
+        <ContentSection />
+        <LocationSection />
+        <Contact />
+      </div>
+    );
+  }
 }
 
 export default App;
